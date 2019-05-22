cb5ae2bbe45de2fa8e407ccaf9a6e4989bb999d5
Pharoouzy <pharoouzy@yahoo.com>
2019-05-22 08:28:37 +0100

    Configured DB and Setup Guard for Admin and User

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+	public function __construct() {
+		$this->middleware('auth:admin');
+	}
+
+	 /**
+     * Show admin dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index() {
+        return view('admin');
+    }
+}
